5b2f843a4a664aa994416085f3e9bc98858fd0b6
janustilt <157038578+janustilt@users.noreply.github.com>
2024-01-18 18:13:20 +0100

    ajout 2 records

--- app.py
+++ app.py
@@ -8,16 +8,38 @@ def get_pod():
     # Récupérer le paramètre "site" depuis la requête du client
     site_param = request.args.get('site')
 
+    qrs = [
+        {
+            'qrcode' : '406f0859-fc2c-4d70-ac29-4b3302049703',
+            'filename' : '123456_AZERTY_20240124180212_OR.TXT',
+            'type' : 'TPM',
+            'rest_home_id' : 123,
+            'logistic_tag' : '1erEtage',
+            'officine' : '159',
+            'site' : 'SPL',
+            'statut' : 'PMI',
+            'creation_date' : '2024-01-24 18:06:12',
+            'qui' : 'addQRcode'
+        },
+            'qrcode' : '66051d36-a268-4292-b6fd-6187aa7871d3',
+            'filename' : '123456_70123456789_20240124181002_OR.TXT',
+            'type' : 'LEGACY',
+            'rest_home_id' : 456,
+            'logistic_tag' : 'cerise',
+            'officine' : '38',
+            'site' : 'LIEGE',
+            'statut' : 'PMI',
+            'creation_date' : '2024-01-24 18:10:12',
+            'qui' : 'addQRcode'
+        }
+    ]
+        
     # Vérifier si le paramètre "site" est fourni
     if site_param is None:
         return jsonify({'error': 'Le paramètre "site" est requis'}), 400
 
     # Générer la réponse JSON en fonction du paramètre "site"
-    response_data = {
-        'site': site_param,
-        'message': f'Données pour le site {site_param}',
-        'autre_information': 'Vos données spécifiques ici',
-    }
+    response_data = qrs
 
     return jsonify(response_data)
 
