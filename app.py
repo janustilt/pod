38bc7421e789d406b962a0921d56b5649ec8de59
janustilt <157038578+janustilt@users.noreply.github.com>
2024-01-18 20:32:09 +0100

    création de getList et setStatutScanned

--- app.py
+++ app.py
@@ -1,101 +1,37 @@
 from flask import Flask, request, jsonify
+from globals import qrs, qrs_lock
 
 app = Flask(__name__)
 
-    qrs = [
-        {
-            'qrcode' : '406f0859-fc2c-4d70-ac29-4b3302049703',
-            'filename' : '123456_AZERTY_20240124180212_OR.TXT',
-            'type' : 'TPM',
-            'rest_home_name' : 'Paix du soir',
-            'logistic_tag' : '1erEtage',
-            'officine' : '159',
-            'site' : 'SPL',
-            'statut' : 'PMI',
-            'creation_date' : '2024-01-24 18:06:12',
-            'qui' : 'addQRcode'
-        },
-        {
-            'qrcode' : '66051d36-a268-4292-b6fd-6187aa7871d3',
-            'filename' : '123456_70123456789_20240124181002_OR.TXT',
-            'type' : 'LEGACY',
-            'rest_home_name' : 'Silence',
-            'logistic_tag' : 'rez',
-            'officine' : '38',
-            'site' : 'LIEGE',
-            'statut' : 'PMI',
-            'creation_date' : '2024-01-24 18:10:12',
-            'qui' : 'addQRcode'
-        },
-        {
-            'qrcode' : 'c1cf5616-7a54-426c-b5b1-8c61e58de0e2',
-            'filename' : '123456_70123456789_20240124181002_OR.TXT',
-            'type' : 'EXTRA',
-            'rest_home_name' : '',
-            'logistic_tag' : '',
-            'officine' : '721',
-            'site' : 'LIEGE',
-            'statut' : 'EXTRA',
-            'creation_date' : '2024-01-24 18:10:12',
-            'qui' : 'addQRcode'
-        },
-        {
-            'qrcode' : '1f19866b-3c48-46a9-ace5-330d564c8aa2',
-            'filename' : '123456_70123456789_20240124181002_OR.TXT',
-            'type' : 'RC',
-            'rest_home_name' : 'ARCADIA',
-            'logistic_tag' : 'top',
-            'officine' : '721',
-            'site' : '721',
-            'statut' : 'RC',
-            'creation_date' : '2024-01-24 8:00:12',
-            'qui' : 'addQRcodeRC'
-        },
-        {
-            'qrcode' : 'ab6d9f53-0d8c-47cb-9af0-fa58af2907d9',
-            'filename' : '123456_70123456789_20240124181002_OR.TXT',
-            'type' : 'RC',
-            'rest_home_name' : 'ARCADIA',
-            'logistic_tag' : 'ciel',
-            'officine' : '721',
-            'site' : '721',
-            'statut' : 'RC',
-            'creation_date' : '2024-01-24 8:00:13',
-            'qui' : 'addQRcodeRC'
-        }
-    ]
-
-
 # Endpoint pour le service "getList"
 @app.route('/getList', methods=['GET'])
-def get_getList():
-    # Récupérer le paramètre "site" depuis la requête du client
+def get_list():
     site_param = request.args.get('site')
-        
-    # Vérifier si le paramètre "site" est fourni
+
     if site_param is None:
         return jsonify({'error': 'Le paramètre "site" est requis'}), 400
 
-    filtered_qrs = [qr for qr in qrs if qr['site'] == site_param]
-    
-    # Générer la réponse JSON en fonction du paramètre "site"
-    response_data = filtered_qrs
+    with qrs_lock:
+        filtered_qrs = [qr for qr in qrs if qr['site'] == site_param]
 
+    response_data = filtered_qrs
     return jsonify(response_data)
 
 # Endpoint pour le service "setStatutScanned"
-@app.route('/setStatutScanned', methods=['GET'])
-def get_setStatutScanned():
-    # Récupérer le paramètre "site" depuis la requête du client
-    qr = request.args.get('qrcode')
-        
-    # Vérifier si le paramètre "qrcode" est fourni
-    if qr is None:
+@app.route('/setStatutScanned', methods=['POST'])
+def set_statut_scanned():
+    qrcode_param = request.json.get('qrcode')
+
+    if qrcode_param is None:
         return jsonify({'error': 'Le paramètre "qrcode" est requis'}), 400
 
-    return jsonify({'result': 'ok'}), 200
+    with qrs_lock:
+        for qr in qrs:
+            if qr['qrcode'] == qrcode_param:
+                qr['statut'] = 'SCANNED'
+                break
 
-
+    return jsonify({'success': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
